50042a39ccfb5f107d99a23b76827230fb5a72c1
itwill <itwill@ITWILLBS04-16>
2019-01-11 20:37:24 +0900

    190111

---
+++ src/main/java/com/semobang/property/domain/PageSearch.java
@@ -0,0 +1,15 @@
+package com.semobang.property.domain;
+
+public class PageSearch {
+	
+	SearchVO svo;
+
+	public SearchVO getSvo() {
+		return svo;
+	}
+
+	public void setSvo(SearchVO svo) {
+		this.svo = svo;
+	}
+
+}
